22ea5d766a5465a6a8a87a14360b3a0acd09f03e
chetan-there <therechetan24@gmail.com>
2026-02-28 15:49:47 +0530

    pfp

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -169,7 +169,7 @@ export const Hero = () => {
               />
               <div className="relative glass rounded-3xl p-2 glow-border">
                 <img
-                  src="/public/pfp.jpg"
+                  src="/pfp.jpg"
                   alt="Chetan There"
                   className="w-full aspect-[4/5] object-cover rounded-2xl"
                 />
